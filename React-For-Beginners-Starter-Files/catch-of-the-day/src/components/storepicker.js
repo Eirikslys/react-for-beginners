e50ec970eac827cef03b4f4f7bfb05e8d80e981f
eirik lysenstoen <elysenstoen@gmail.com>
2021-01-11 14:11:35 +0100

    comments and fragments

--- React-For-Beginners-Starter-Files/catch-of-the-day/src/components/storepicker.js
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/components/storepicker.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
 class StorePicker extends React.Component {
   render() {
     return (
-      <p>Fish!</p>
-      <form className="store-selector">
-        <h2> Please Enter A Store</h2>
-      </form>
+      <Fragment>
+        { /* comment */ }
+        <p>Fish!</p>
+        <form className="store-selector">
+          <h2> Please Enter A Store</h2>
+          <input type="text" required placeholder ="Store Name" />
+          <button type="submit"> Visit Store -> </button>
+        </form>
+      </Fragment>
     )
   }
 }
