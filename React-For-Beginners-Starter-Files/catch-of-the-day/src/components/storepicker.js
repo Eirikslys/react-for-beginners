2faa7800c8936b525e389831da0cd4c20fbd9777
eirik lysenstoen <elysenstoen@gmail.com>
2021-01-11 13:03:36 +0100

    Fiddling with JSX

---
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/components/storepicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <p>Fish!</p>
+      <form className="store-selector">
+        <h2> Please Enter A Store</h2>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
