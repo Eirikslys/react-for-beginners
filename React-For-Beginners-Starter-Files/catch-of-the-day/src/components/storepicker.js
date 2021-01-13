9bba5e6193f3fe40c29fed1edf027fedbaf9a2f1
eirik lysenstoen <elysenstoen@gmail.com>
2021-01-13 14:24:07 +0100

    linking pages

--- React-For-Beginners-Starter-Files/catch-of-the-day/src/components/storepicker.js
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/components/storepicker.js
@@ -1,15 +1,33 @@
 import React, { Fragment } from 'react';
-
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = event => {
+    // 1. stop the form from submitting
+    event.preventDefault();
+    // console.log(event);
+    // 2. get the text from that input
+    console.log(this.myInput.current.value)
+    // 3. change the page to the store/whatever
+  }
+  // componentDidMount() {
+  //   console.log("Mounted")
+  //   console.log(this)
+  // }
   render() {
     return (
       <Fragment>
+      <p>Fish!</p>
         { /* comment */ }
-        <p>Fish!</p>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2> Please Enter A Store</h2>
-          <input type="text" required placeholder ="Store Name" />
+          <input
+            type="text"
+            ref={this.myInput}
+            required placeholder ="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit"> Visit Store -> </button>
         </form>
       </Fragment>
